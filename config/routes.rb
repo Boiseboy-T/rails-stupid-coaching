0f8e0f16c932381b9f7f0a8a4c6631fc44471be7
takaakinakagami <takaaki0120n@gmail.com>
2023-02-13 15:51:12 +0900

    stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'questions#ask'
+  get '/ask', to: 'questions#ask', as: :question
+  get '/answer', to: 'questions#answer', as: :answer
+
 end
